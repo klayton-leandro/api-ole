7429271cefa58e17199baa6acb03c2178ea28582
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-10-30 16:15:36 -0300

    Altera rota

--- start/routes.js
+++ start/routes.js
@@ -4,4 +4,5 @@ const Route = use("Route");
 
 Route.post("users", "UserController.store");
 Route.post("sessions", "SessionController.store");
-Route.post("files/:id", "FileController.show");
+
+Route.get("users/:id/files", "FileController.show");
