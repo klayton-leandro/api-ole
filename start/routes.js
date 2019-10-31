bddf62c50eb3a18b70b150fc3822c2e02e3641f7
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-10-31 15:07:14 -0300

    Atualiza File

--- start/routes.js
+++ start/routes.js
@@ -5,4 +5,9 @@ const Route = use("Route");
 Route.post("users", "UserController.store");
 Route.post("sessions", "SessionController.store");
 
-Route.get("users/:id/files", "FileController.show");
+Route.get("files/:id", "FileController.show");
+
+Route.group(() => {
+  Route.get("files", "FileController.index");
+  Route.put("files/:id", "FileController.update");
+}).middleware(["auth"]);
